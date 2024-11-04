caf619929ff37dabad5b136903a05e8eb3e5a7ec
GabrielvMarinho <147559808+GabrielvMarinho@users.noreply.github.com>
2024-11-04 15:48:46 -0300

    modais completos e estilização deles pronta

--- flask/projeto/static/saldoModal.js
+++ flask/projeto/static/saldoModal.js
@@ -1,16 +1,15 @@
 // Get modal and close button elements
 const modalDeposito = document.getElementById("myModalDepo");
 const spanDepo = document.getElementsByClassName("closeDepo")[0];
-console.log(modalDeposito)
-console.log(spanDepo)
+
+
 // Function to show the modal
 function adicionarSaldoModal() {
     modalDeposito.classList.add("show");
 }
 
 spanDepo.onclick = function() {
-  document.getElementsByClassName("overlay")[0].classList.remove("escuro");
-  modalconf.style.display = "none";
+    modalDeposito.classList.remove("show");
 }
 
 // Close the modal if the user clicks outside it
