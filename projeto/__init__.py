2a44d8ee3fd81ff352003dbe2ae2f2decc6cbd81
GabrielvMarinho <147559808+GabrielvMarinho@users.noreply.github.com>
2024-11-14 21:58:54 -0300

    adicionado inicio config email

--- projeto/__init__.py
+++ projeto/__init__.py
@@ -9,6 +9,7 @@ login_manager = LoginManager()
 login_manager.login_message = "Por favor, faça login para acessar esta página!"
 login_manager.login_view = 'login'
 app = Flask(__name__)
+app.config.from_pyfile("config.cfg")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SECRET_KEY"] = '934fhdr58g6jj643dg4d55fg'
 db.init_app(app)
